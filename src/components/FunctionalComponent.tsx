b7668ad06274d30e6d1d3f0977a424e2f42193a6
Julia Lee <julee@pivotal.io>
2019-10-15 14:17:47 -0500

    inline function for functional component

--- src/components/FunctionalComponent.tsx
+++ src/components/FunctionalComponent.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-const logRandom = () => console.log(Math.random());
-
 export function Button() {
   const [counter, setCounter] = React.useState(7);
-  return <button onClick={logRandom}>{counter}</button>;
+  return <button onClick={() => console.log(Math.random())}>{counter}</button>;
 }
